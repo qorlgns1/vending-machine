16d637f34a0ba1efc10c88e7c34788dd24ad33c6
qorlgns1 <rlgns0610@gmail.com>
2022-04-16 19:47:53 +0900

    feat: 콜라버튼에서 콜라의 이름으로 인덱스를 뽑기위한 함수 추가

--- js/common/extract.js
+++ js/common/extract.js
@@ -1,3 +1,12 @@
 export const extractNum = (num) => {
   return num.toString().replace(/[^0-9]/g, "");
 };
+
+export const getColaIndexNode = (selector, cola) => {
+  var elem = document.querySelectorAll(selector);
+  for (var i = 0; i < elem.length; i++) {
+    if (elem[i].children[1].innerHTML === cola) {
+      return i;
+    }
+  }
+};
